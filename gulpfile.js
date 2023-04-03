b2933713d5ec9f855549c5f6cd9ec941b41b5d7d
Fernando Cuellar <fercuellar@gmail.com>
2023-04-02 21:44:10 -0600

    se compila sass con gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const {src, dest} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+    // compilar sass
+    // pasos: 1 - Identificar, 2 - Compilar, 3 - Guardar el .css
+
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('build/css'))
+    done();
+}
+
+exports.css = css;
