a7c81042e03b4a7dac8c6a2c499709398fb894f4
Fernando Cuellar <fercuellar@gmail.com>
2023-04-02 22:26:26 -0600

    se añadio un autoprefier

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done) {
     // compilar sass
     // pasos: 1 - Identificar, 2 - Compilar, 3 - Guardar el .css
 
     src('src/scss/app.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
         .pipe(dest('build/css'))
     done();
 }
