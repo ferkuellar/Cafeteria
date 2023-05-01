c19f525c5bb694615f222a9ca0a9c7b2fc86dbf2
Fernando Cuellar <fercuellar@gmail.com>
2023-05-01 00:46:36 -0600

    se modificario y se reparo package-json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 const {src, dest, watch, series, parallel} = require('gulp');
+
+// dependencias CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     // compilar sass
@@ -14,14 +22,45 @@ function css(done) {
     done();
 }
 
-function dev() {
-    watch('src/scss/**/*.scss', css);
+function imagenes(done){
+    src('src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'));
+    done();
+}
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg,jpeg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'));
+done();
 }
 
 
+function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 // exports.default = parallel(css, dev);
 
 // series- ejecuta la primer tarea y una vez que la completa se va a la siguente tarea
